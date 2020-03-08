d22d0d9e921e60d0362287654569f89199c3fb1c
Alan Chandler <alan@chandlerfamily.org.uk>
2020-03-08 12:51:31 +0000

    Strip out unused stuff from server

--- server/server.js
+++ server/server.js
@@ -59,164 +59,194 @@
   let server;
   const recorders = {};
   let statusid = 0;
+  let channelid = 1;
   const subscribedChannels = {};
+  let statusTimer = 0;
 
   async function startUp (http2, Router,enableDestroy, logger, Recorder, usbDetect) {
-    const routerOpts = {mergeParams: true};
-    const router = Router(routerOpts);  //create a router
+    try {
+      const routerOpts = {mergeParams: true};
+      const router = Router(routerOpts);  //create a router
 
-    const options = {
-      key: await fs.readFile(path.resolve(__dirname,  'assets/key.pem')),
-      cert: await fs.readFile(path.resolve(__dirname,  'assets/certificate.pem')),
-      allowHttp1: true
-    };
-    debug('have server ssl keys about to create the http2 server')
-    server = http2.createSecureServer(options, (req,res) => {
-      const reqURL = url.parse(req.url).pathname;
-      if (reqURL.indexOf('volume') < 0) {
-        debugfile('request for ', reqURL, ' received');
-      } else {
-        debugmedia('request for ', reqURL, 'received');
-      } 
-      function final(err) {
-        if (err) {
-          logger('url','Request Error ' + (err.stack || err.toString()));
+      const options = {
+        key: await fs.readFile(path.resolve(__dirname,  'assets/key.pem')),
+        cert: await fs.readFile(path.resolve(__dirname,  'assets/certificate.pem')),
+        allowHttp1: true
+      };
+      debug('have server ssl keys about to create the http2 server')
+      server = http2.createSecureServer(options, (req,res) => {
+        const reqURL = url.parse(req.url).pathname;
+        if (reqURL.indexOf('volume') < 0) {
+          debugfile('request for ', reqURL, ' received');
         } else {
-          
-          const ip = req.remoteAddress;
-          const hostname = req.headers.host;
-          logger('url','Request for ' + reqURL + ' from ' + hostname + ' not found',ip);
+          debugmedia('request for ', reqURL, 'received');
+        } 
+        function final(err) {
+          if (err) {
+            logger('url','Request Error ' + (err.stack || err.toString()));
+          } else {
+            logger('url','Request for ' + reqURL + ' not found');
+          }
+          //could not find so send a 404
+          res.statusCode = 404;
+          res.end();
         }
-        //could not find so send a 404
-        res.statusCode = 404;
-        res.end();
-      }
-      router(req, res, err => {
-        if (err) {
-          final(err);
-        } else if (/^\/(api(\/(.*)|))$/.test(reqURL)) {
-            /*
-              If we get here it means that it is not a route created by the client, and so we should
-              respond with a 404.
-            */
-            debug(reqURL,' is one which should normally have been handled already, so now its a 404');
-            final();
+        router(req, res, final);
+
+      });
+      server.listen(parseInt(process.env.RECORDER_PORT,10),'0.0.0.0');
+      enableDestroy(server);
+      
+      router.get('/api/recording/:id/take',checkRecorder, (req,res) => {
+        debug('take request received');
+        const token = req.recorder.take();
+        res.statusCode = 200;
+        if (token) {
+          res.end(JSON.stringify({state: true, token: token}));
+          sendStatus('take',{ id: req.recorder.name});
         } else {
-          /*
-            This is a url that should not have been handled by known server routes.  This means that
-            it is likely that its a client generated url and so we should respond with index.html
-          */
-          debug('app route of %s called sending index.html',reqURL);
-          req.url = '/index.html';
-          serveFile(req, res, final);
+          res.end(JSON.stringify({state: false, token: ''}));
         }
       });
+      router.get('/api/recording/:id/renew/:token', checkRecorder,(req,res) => {
+        debug('take request received');
+        const token = req.recorder.renew(req.params.token);
+        res.statusCode = 200;
+        if (token) {
+          res.end(JSON.stringify({state: true, token: token}));
+        } else {
+          res.end(JSON.stringify({state: false, token: ''}));
+        }
+      });
+      router.get('/api/recording/:id/release/:token', checkRecorder, (req,res) => {
+        const state = req.recorder.release(req.params.token)
+        res.end(JSON.stringify({state: state}));
+        if (state) {
+          sendStatus('release', req.recorder.name);
+        }
+      });
+      router.get('/api/recording/:id/start/:token', checkRecorder, (req,res) => {
+        debug('got a start request with params ', req.params);
+        res.end(JSON.stringify({state: req.recorder.record(req.params.token)}));
+      });
+      router.get('/api/recording/:id/stop/:token', checkRecorder, (req,res) => {
+        debug('got a stop request with params ', req.params);
+        res.end(JSON.stringify({state: req.recorder.stop(req.params.token)}));
+      });
+      router.get('/api/status/:channel', (req,res) => {
+        if (req.headers.accept && req.headers.accept == 'text/event-stream') {
+          const channel = req.params.channel;
+          debug('/api/status received creating/reusing channel ', channel.toString());
+          subscribedChannels[channel] = res;
+          res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive'
+          });
+          debug('wrote headers');
+          req.once('end', () => {
+            debug('client closed status channel ', channel.toString());
+            delete subscribedChannels[channel];
+          });
+          const status = {
+            scarlett: {
+              connected: recorders.scarlett !== undefined,
+              taken: recorders.scarlett !== undefined ? recorders.scarlett.controlled : false,
+              name: recorders.scarlett !== undefined ? recorders.scarlett.name : ''
+            },
+            yeti: {
+              connected: recorders.yeti !== undefined,
+              taken: recorders.yeti !== undefined ? recorders.yeti.controlled : false,
+              name: recorders.yeti !== undefined ? recorders.yeti.name : ''
+            }
+          }
+          sendStatus('status', status, channel);
 
-    });
-    server.listen(parseInt(process.env.RECORDER_PORT,10),'0.0.0.0');
-    enableDestroy(server);
+        } else {
+          res.writeHead(404);
+          res.end();
+        }
+      });
+      router.use('/', serveFile);
+
+      usbDetect.on('add:' + process.env.RECORDER_SCARLETT_VID + ':' + process.env.RECORDER_SCARLETT_PID, device => {
+        recorders.scarlett = new Recorder(process.env.RECORDER_SCARLETT_HW, device.deviceName, 'volumes');
+        sendStatus('add', {id: 'scarlett', name: recorders.scarlett.name});
+        debug('created scarlett recorder');    
+      });
+      usbDetect.on('add:' + process.env.RECORDER_YETI_VID + ':' + process.env.RECORDER_YETI_PID, device => {
+        recorders.yeti = new Recorder(process.env.RECORDER_YETI_HW, device.deviceName, 'volumey');
+        sendStatus('add', {id: 'yeti', name: recorders.yeti.name});
+        debug('created yeti recorder');
+      });
+      usbDetect.on('remove:' + process.env.RECORDER_SCARLETT_VID + ':' + process.env.RECORDER_SCARLETT_PID, async device => {
+        debug('about to close scarlett recorder');
+        await recorders.scarlett.close() //stop the recorder
+        sendStatus('remove', {id: 'scarlett'});
+        delete recorders.scarlett;
+        debug('closed scarlett recorder');
+      });
+      usbDetect.on('remove:' + process.env.RECORDER_YETI_VID + ':' + process.env.RECORDER_YETI_PID, async () => {
+        debug('about to close yeti recorder');
+        await recorders.yeti.close();
+        sendStatus('remove', {id: 'yeti'});
+        delete recorders.yeti;
+        debug('closed yeti recorder');
+      });
+
+      //start looking for udev events
+      usbDetect.startMonitoring();
+
+
+      //lets see if anthing plugged in and set initial state
+      const devices = await Promise.all([
+        usbDetect.find(parseInt(process.env.RECORDER_SCARLETT_VID,10),parseInt(process.env.RECORDER_SCARLETT_PID,10)),
+        usbDetect.find(parseInt(process.env.RECORDER_YETI_VID,10),parseInt(process.env.RECORDER_YETI_PID,10))
+      ]);
+
+      debug('currently connected devices ', devices);
+      if (devices[0].length > 0) {
+        recorders.scarlett = new Recorder(process.env.RECORDER_SCARLETT_HW, devices[0][0].deviceName, 'volumes');
+      }
+      if (devices[1].length > 0) {
+        recorders.yeti = new Recorder(process.env.RECORDER_YETI_HW, devices[1][0].deviceName, 'volumey');
+      }
+      const status = {
+        scarlett: {
+          connected: recorders.scarlett !== undefined,
+          taken: recorders.scarlett !== undefined ? recorders.scarlett.controlled : false,
+          name: recorders.scarlett !== undefined ? recorders.scarlett.name : ''
+        },
+        yeti: {
+          connected: recorders.yeti !== undefined,
+          taken: recorders.yeti !== undefined ? recorders.yeti.controlled : false,
+          name: recorders.yeti !== undefined ? recorders.yeti.name : ''
+        }
+      };
+      sendStatus('status', status);
+      statusTimer = setInterval(() => {
+        const status = {
+          scarlett: {
+            connected: recorders.scarlett !== undefined,
+            taken: recorders.scarlett !== undefined ? recorders.scarlett.controlled : false,
+            name: recorders.scarlett !== undefined ? recorders.scarlett.name : ''
+          },
+          yeti: {
+            connected: recorders.yeti !== undefined,
+            taken: recorders.yeti !== undefined ? recorders.yeti.controlled : false,
+            name: recorders.yeti !== undefined ? recorders.yeti.name : ''
+          }
+        };
+        sendStatus('status', status);
     
-    router.get('/api/recording/:id/take',checkRecorder, (req,res) => {
-      debug('take request received');
-      const token = req.recorder.take();
-      res.statusCode = 200;
-      if (token) {
-        res.end(JSON.stringify({state: true, token: token}));
-      } else {
-        res.end(JSON.stringify({state: false, token: ''}));
-      }
-    });
-    router.get('/api/recording/:id/renew/:token', checkRecorder,(req,res) => {
-      debug('take request received');
-      const token = req.recorder.renew(req.params.token);
-      res.statusCode = 200;
-      if (token) {
-        res.end(JSON.stringify({state: true, token: token}));
-      } else {
-        res.end(JSON.stringify({state: false, token: ''}));
-      }
-    });
-    router.get('/api/recording/:id/release/:token', checkRecorder, (req,res) => {
-      res.end(JSON.stringify({state: req.recorder.release(req.params.token)}));
-    });
-    router.get('/api/recording/:id/start/:token', checkRecorder, (req,res) => {
-      debug('got a start request with params ', req.params);
-      res.end(JSON.stringify({state: req.recorder.record(req.params.token)}));
-    });
-    router.get('/api/recording/:id/stop/:token', checkRecorder, (req,res) => {
-      debug('got a stop request with params ', req.params);
-      res.end(JSON.stringify({state: req.recorder.stop(req.params.token)}));
-    });
-    router.get('/api/status', (req,res) => {
-      const channel = Symbol();
-      debug('/api/status received creating chanel ', channel,toString());
-      subscribedChannels[channel] = res;
-      res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive'
-      });
-      debug('wrote headers');
-      req.once('end', () => {
-        debug('client closed status channel ', channel.toString());
-        delete subscribedChannels[channel];
-      });
-      if (recorders.scarlett !== undefined) {
-        sendStatus('add', {id: 'scarlett', name: recorders.scarlett.name}, channel);
-        if (recorders.scarlett.controlling) sendStatus('taken', {id: 'scarlett'}, channel);
-      }
-      if (recorders.yeti !== undefined) {
-        sendStatus('add', {id:'yeti', name: recorders.yeti.name}, channel);
-        if (recorders.yeti.controlling) sendStatus('taken', {id: 'yeti'}, channel);
-      }
+      }, 90000);
 
-    });
-    router.use('/', serveFile);
-
-    usbDetect.on('add:' + process.env.RECORDER_SCARLETT_VID + ':' + process.env.RECORDER_SCARLETT_PID, device => {
-      recorders.scarlett = new Recorder(process.env.RECORDER_SCARLETT_HW, device.deviceName, 'volumes');
-      sendStatus('add', {id: 'scarlett', name: recorders.scarlett.name});
-      debug('created scarlett recorder');    
-    });
-    usbDetect.on('add:' + process.env.RECORDER_YETI_VID + ':' + process.env.RECORDER_YETI_PID, device => {
-      recorders.yetti = new Recorder(process.env.RECORDER_YETI_HW, device.deviceName, 'volumey');
-      sendStatus('add', {id: 'yeti', name: recorders.yeti.name});
-      debug('created yeti recorder');
-    });
-    usbDetect.on('remove:' + process.env.RECORDER_SCARLETT_VID + ':' + process.env.RECORDER_SCARLETT_VID, async device => {
-      debug('about to close scarlett recorder');
-      await recorders.scarlett.close() //stop the recorder
-      sendStatus('remove', {id: 'scarlett'});
-      delete recorders.scarlett;
-      debug('closed scarlett recorder');
-    });
-    usbDetect.on('remove:' + process.env.RECORDER_YETI_VID + ':' + process.env.RECORDER_YETI_PID, async () => {
-      debug('about to close yeti recorder');
-      await recorders.yeti.close();
-      sendStatus('remove', {id: 'yeti'});
-      delete recorders.yeti;
-      debug('closed yeti recorder');
-    });
-
-    //start looking for udev events
-    usbDetect.startMonitoring();
-
-
-    //lets see if anthing plugged in and set initial state
-    const devices = await Promise.all([
-      usbDetect.find(parseInt(process.env.RECORDER_SCARLETT_VID,10),parseInt(process.env.RECORDER_SCARLETT_PID,10)),
-      usbDetect.find(parseInt(process.env.RECORDER_YETI_VID,10),parseInt(process.env.RECORDER_YETI_PID,10))
-    ]);
-
-    debug('currently connected devices ', devices);
-    if (devices[0].length > 0) {
-      recorders.scarlett = new Recorder(process.env.RECORDER_SCARLETT_HW, devices[0][0].deviceName, 'volumes');
+      logger('app', 'Recorder Web Server Operational Running on Port:' +
+          process.env.RECORDER_PORT + ' with Node Version: ' + process.version);
+    } catch(err) {
+      logger('error', err);
+      close();
     }
-    if (devices[1].length > 0) {
-      recorders.yeti = new Recorder(process.env.RECORDER_YETI_HW, devices[1][0].deviceName, 'volumey');
-    }
-    logger('app', 'Recorder Web Server Operational Running on Port:' +
-        process.env.RECORDER_PORT + ' with Node Version: ' + process.version);
   }
   function checkRecorder(req, res, next) {
     debug('recording router id = ', req.params.id);
@@ -306,33 +336,39 @@
     statusid++;
     res.write(`id: ${statusid.toString()}\n`);
     res.write(`event: ${type}\n`);
-    res.write('retry: 10000\n');
+//    res.write('retry: 10000\n');
     res.write("data: " + JSON.stringify(data) + '\n\n');
+    debug('message sent with data  ', data);
   }
 
   async function close() {
   // My process has received a SIGINT signal
     if (server) {
-      sendStatus('close',{});
-      let tmp = server;
-      server = null;
-      debug('about to stop monitoring udev events')
-      await usbDetect.stopMonitoring();
-      if (recorders.scarlett !== undefined) {
-        //need to shut off the recording smoothly
-        debug('stopping scarlett');
-        await recorders.scarlett.close();
-        delete recorders.scarlett;
-      }
-      if (recorders.yeti !== undefined) {
-        debug('stopping yetti');
-        await recordery.close();
-        delete recorders.yeti;
-      }
+      try {
+        if (statusTimer !== 0) clearInterval(statusTimer);
+        sendStatus('close',{});
+        let tmp = server;
+        server = null;
+        debug('about to stop monitoring udev events')
+        await usbDetect.stopMonitoring();
+        if (recorders.scarlett !== undefined) {
+          //need to shut off the recording smoothly
+          debug('stopping scarlett');
+          await recorders.scarlett.close();
+          delete recorders.scarlett;
+        }
+        if (recorders.yeti !== undefined) {
+          debug('stopping yetti');
+          await recorders.yeti.close();
+          delete recorders.yeti;
+        }
 
-      debug('About to close Web Server');
-      tmp.destroy();
-      logger('app', 'Recorder Server ShowDown')
+        debug('About to close Web Server');
+        tmp.destroy();
+        logger('app', 'Recorder Server ShowDown');
+      } catch (err) {
+        logger('error', error);
+      }
     }
     process.exit(0);
   }
